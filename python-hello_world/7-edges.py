f3e501e8898ed5550a4c0a46c8613e82cfbff675
Bryan Garcia <bags@Bryans-MacBook-Air.local>
2024-05-07 11:11:00 -0400

    checking code

--- python-hello_world/7-edges.py
+++ python-hello_world/7-edges.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 word_first_3 = "Hol"
 word_last_2 = "on"
-middle_word = "olberton"
+middle_word = "olberto"
 print(f"First 3 letter: {word_first_3}")
 print(f"Last 2 letters: {word_last_2}")
 print(f"Middle word: {middle_word}")
