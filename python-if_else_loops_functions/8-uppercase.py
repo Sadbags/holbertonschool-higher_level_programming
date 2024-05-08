2e250cb9d62e116fae23a0b618f03fe1e1865312
Bryan Garcia <bags@Bryans-MacBook-Air.local>
2024-05-08 11:29:57 -0400

    checks for uppercase

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def uppercase(str):
-    for char in str:
-        upper_char = chr(ord(char) - 32) if 97 <= ord(char) <= 122 else char
-        print(upper_char, end="")
-    print()
+    upstr = ""
+    for i in str:
+        if ord(i) >= ord('a') and ord(i) <= ord('z'):
+            upstr += chr(ord(i) - 32)
+        else:
+            upstr += i
+    print('{}'.format(upstr))
