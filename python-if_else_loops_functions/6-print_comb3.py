e38e6d773bc43f1b14744eb8f6caa43459d217cb
Bryan Garcia <bags@Bryans-MacBook-Air.local>
2024-05-08 11:13:15 -0400

    print number 01 - 89

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-for i in range(10):
-    for j in range(i+1, 10):
-        print("{:d}{:d},".format(i, j), end="" if i == 9 and j == 8 else " ")
-print()
+#!/usr/bin/python3
+for number in range(0, 9):
+    for number2 in range(number + 1, 10):
+        if number == 8 and number2 == 9:
+            print("{}{}".format(number, number2))
+        else:
+            print("{}{}".format(number, number2), end=", ")
