2e719b8bb604045e6fe77259df7cb3ef6ee8db7a
Bryan Garcia <bags@Bryans-MacBook-Air.local>
2024-05-13 12:27:06 -0400

    added file

--- python-data_structures/12-switch.py
+++ python-data_structures/12-switch.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
-a = 89
-b = 10
-a, b = b, a
-print("a={:d} - b={:d}".format(a, b))
+def add_tuple(tuple_a=(), tuple_b=()):
+    # If tuple_a or tuple_b is smaller than 2, add zeros for missing integers
+    if len(tuple_a) < 2:
+        tuple_a += (0, ) * (2 - len(tuple_a))
+    if len(tuple_b) < 2:
+        tuple_b += (0, ) * (2 - len(tuple_b))
+
+    # Add the first elements of the tuples and the second elements of the tuples
+    sum_first = tuple_a[0] + tuple_b[0]
+    sum_second = tuple_a[1] + tuple_b[1]
+
+    # Return the tuple containing the sums
+    return (sum_first, sum_second)
