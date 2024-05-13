edbed4c01010160e1b89c2e8a08479fc56dec852
Bryan Garcia <bags@Bryans-MacBook-Air.local>
2024-05-13 12:10:31 -0400

    added file

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,15 +1,5 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    new_tuple = [0, 0]
-
-    for a in range(2):
-        try:
-            new_tuple[a] += tuple_a[a]
-        except:
-            new_tuple[a] += 0
-        try:
-            new_tuple[a] += tuple_b[a]
-        except:
-            new_tuple[a] += 0
-
-    return tuple(new_tuple)
+	a = tuple_a + (0, 0)
+	b = tuple_b + (0, 0)
+	return (a[0] + b[0], a[1] + b[1])
