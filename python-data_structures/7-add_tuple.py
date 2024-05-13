c27f73fda2fadf57a4be1364d36a9e22c3591040
Bryan Garcia <bags@Bryans-MacBook-Air.local>
2024-05-13 12:31:49 -0400

    trying to fix check

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    # If tuple_a or tuple_b is smaller than 2, add zeros for missing integers
-    if len(tuple_a) < 2:
-        tuple_a += (0, ) * (2 - len(tuple_a))
-    if len(tuple_b) < 2:
-        tuple_b += (0, ) * (2 - len(tuple_b))
+    new_tuple = [0, 0]
 
-    # Add the first elements of the tuples and the second elements of the tuples
-    sum_first = tuple_a[0] + tuple_b[0]
-    sum_second = tuple_a[1] + tuple_b[1]
+    for a in range(2):
+        try:
+            new_tuple[a] += tuple_a[a]
+        except:
+            new_tuple[a] += 0
+        try:
+            new_tuple[a] += tuple_b[a]
+        except:
+            new_tuple[a] += 0
 
-    # Return the tuple containing the sums
-    return (sum_first, sum_second)
+    return tuple(new_tuple)
