3fb3c03ef197c9acac11caff396fd04decfc97d1
Bryan Garcia <bags@Bryans-MacBook-Air.local>
2024-05-14 10:45:50 -0400

    added file

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-    new_matrix = []
-    rows, cols = (6, 6)
-    for i in range(rows):
-        new_matrix.append([])
-        for j in range(cols):
-            new_matrix[i].append(matrix[i][j] * matrix[i][j])
-            print(new_matrix[i][j], end=" ")
+    for i in range(len(matrix)):
+        new_matrix = [[i*i for i in j] for j in matrix]
+    return new_matrix
