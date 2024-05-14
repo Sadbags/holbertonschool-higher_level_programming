285bf20166e9ca750f6a8d94b9ad19bd081840f5
Bryan Garcia <bags@Bryans-MacBook-Air.local>
2024-05-14 11:03:48 -0400

    added file

---
+++ python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def uniq_add(my_list=[]):
+    unique_integers = set()
+    for element in my_list:
+        if isintance(element, int):
+            unique_integers.add(element)
+    total = sum(unique_integers)
+    return total
