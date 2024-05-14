dc86b14be71d24626a7d28fb01f147237e616062
Bryan Garcia <bags@Bryans-MacBook-Air.local>
2024-05-14 11:28:36 -0400

    added file to see checks

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -2,7 +2,3 @@
 def simple_delete(a_dictionary, key=""):
     if key in a_dictionary:
         del a_dictionary[key]
-
-a_dictionary = {'a': 1, 'b': 2, 'c': 3}
-key_to_delete = 'b'
-print(simple_delete(a_dictionary, key_to_delete))
