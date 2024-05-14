ad8e74417ae268f39bcbd21360be683b9171a4c5
Bryan Garcia <bags@Bryans-MacBook-Air.local>
2024-05-14 11:50:59 -0400

    file

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -3,7 +3,7 @@ def roman_to_int(roman_string):
     if not isinstance(roman_string, str) or roman_string is None:
         return 0
 
-    roman_numerals = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    roman_numerals = {'I': 1, 'III': 3, 'XXI': 21, 'IV': 4, 'CXXIV': 124, 'XCIX': 99, 'LXXXIX': 89}
     result = 0
 
     for i in range(len(roman_string)):
