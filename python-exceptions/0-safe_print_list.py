3525b0d575325b94d41b4d555ed9f15bd99d00c4
Bryan Garcia <bags@Bryans-MacBook-Air.local>
2024-05-15 10:32:35 -0400

    added file

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,21 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-	try:
-		nb_print = 0
-		for i in range(x):
-			print("{}".format(my_list[i]), end="")
-			nb_print += 1
-		print()
-		return nb_print
-	except IndexError:
-		print()
-		return nb_print
-	except TypeError:
-		print()
-		return nb_print
-	except ValueError:
-		print()
-		return nb_print
-	except:
-		print()
-		return nb_print
+    c = 0
+    try:
+        for i in range(0, x):
+            print(my_list[i], end+"")
+            c += 1
+    except IndexError:
+        pass
+    print()
+    return c
