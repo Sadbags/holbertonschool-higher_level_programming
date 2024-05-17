ec416f9a44a5f5f47d086049c96fa7303868187a
Bryan Garcia <bags@Bryans-MacBook-Air.local>
2024-05-17 00:06:45 -0400

    trying to get checks right

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+""" Define Square module """
+
 
 class Square:
     """Represent a Square."""
@@ -13,4 +15,4 @@ class Square:
 
     def area(self):
         """Return the area of the square."""
-        return self.__size ** 2
+        return self.__size * self.__size
