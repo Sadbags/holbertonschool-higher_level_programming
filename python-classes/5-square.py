d91028c783ff039a32be7f3fc42424dcc171a5f4
Bryan Garcia <bags@Bryans-MacBook-Air.local>
2024-05-17 00:34:33 -0400

    first commit

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -1,31 +1,49 @@
 #!/usr/bin/python3
-""" Define square module"""
+""" Module with the class Square """
 
 
 class Square:
-    """ Define Square """
+    """ Square Defined """
+    __size = None
 
     def __init__(self, size=0):
+        """ Docstring of __init___ method
+
+        Args:
+            size (int): size from main to be displayed
+        """
         self.__size = size
+        """ int: Docstring *after* attribute, with type specified """
 
-        @property
-        def size(self):
-            return self.__size
+    @property
+    def size(self):
+        """ Docstring of size """
+        return self.__size
+        """ returns the size attribute with value """
 
-            @size.setter
-            def size(self, value):
-                if (type(value) != int):
-                    raise TypeError("size must be an integer")
-                if (value < 0):
-                    raise ValueError("size must be >= 0")
-            self.__size = value
+    @size.setter
+    def size(self, value):
+        """ Docstring of size
 
-        def area(self):
-            return self.__size * self.__size
+        Args:
+            value (int): Contains size from __size attribute
+        """
+        if (type(value) != int):
+            raise TypeError("size must be an integer")
+        if (self.__size < 0):
+            raise ValueError("size must be >= 0")
+        self.__size = value
+        """ set size """
 
-            def my_print(self):
-                if self.__size == 0:
-                    print()
-                else:
-                    for i in range(self.__size):
-                        print("#" * self.__size)
+    def area(self):
+        """ Docstring of area """
+        return self.__size * self.__size
+        """ size squared """
+
+    def my_print(self):
+        """ Docstring of my_print printer """
+        if (self.__size == 0):
+            print()
+        else:
+            for x in range(self.__size):
+                print("#" * self.__size)
