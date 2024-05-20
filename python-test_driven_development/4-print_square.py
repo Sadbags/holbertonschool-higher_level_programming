1cd85a4316f78a8f9f394fb26004e9c9115f5715
Bryan Garcia <bags@Bryans-MacBook-Air.local>
2024-05-20 12:02:33 -0400

    added file for checks

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Function that prints a square with the #
+"""
+
+
+def print_square(size):
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for i in range(size):
+        for j in range(size):
+            print("#", end="")
+        print()
