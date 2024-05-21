0c279062d27221dd979a2ec896dc8b5a1e6dc377
Bryan Garcia <bags@Bryans-MacBook-Air.local>
2024-05-21 05:12:20 -0400

    checking las checker

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -1,23 +1,29 @@
 #!/usr/bin/python3
-""" class Rectangle module"""
+"""
+2-rectangle.py
+
+This module defines a class Rectangle with
+private attributes for width and height,
+and methods to calculate the area and perimeter of the rectangle.
+"""
 
 
 class Rectangle:
-    """ Define a rectangle """
-    __width = None
-    __height = None
+    """Class that defines a rectangle."""
 
     def __init__(self, width=0, height=0):
-        self.height = height
         self.width = width
+        self.height = height
 
     @property
     def width(self):
+        """Getter method for the width attribute."""
         return self.__width
 
     @width.setter
     def width(self, value):
-        if type(value) != int:
+        """Setter method for the width attribute."""
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -25,21 +31,24 @@ class Rectangle:
 
     @property
     def height(self):
+        """Getter method for the height attribute."""
         return self.__height
 
     @height.setter
     def height(self, value):
-        if type(value) != int:
+        """Setter method for the height attribute."""
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
 
     def area(self):
+        """Calculate the area of the rectangle."""
         return self.__width * self.__height
 
     def perimeter(self):
-
+        """Calculate the perimeter of the rectangle."""
         if self.__width == 0 or self.__height == 0:
             return 0
-        return (self.__width * 2) + (self.__height * 2)
+        return 2 * (self.__width + self.__height)
