63cffe7c12c959eead6ae6f15c6f750f3dfe1cfb
Bryan Garcia <bags@Bryans-MacBook-Air.local>
2024-05-22 03:35:58 -0400

    added file

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -5,7 +5,7 @@
 def inherits_from(obj, a_class):
     """ inheritance """
 
-    if issubclass(type(obj), a_class) and type(obj) is a_class:
+    if issubclass(type(obj), a_class) and not type(obj) is a_class:
         return True
     else:
         return False
