9a60940c76b8c0b37843312db5a7b344a616b323
Bryan Garcia <bags@Bryans-MacBook-Air.local>
2024-05-22 03:49:20 -0400

    added file

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" module that contains a class """
+
+
+class BaseGeometry:
+    """ raise exception """
+
+    def area(self):
+        raise Exception("area() is not implemented")
