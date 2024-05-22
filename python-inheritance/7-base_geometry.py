716a5ae6c1c4bb07550fee11b40b60db49cd96d5
Bryan Garcia <bags@Bryans-MacBook-Air.local>
2024-05-22 04:01:55 -0400

    added file

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" module of a class """
+
+
+class BaseGeometry:
+    """ raise exception """
+
+    def area(self):
+        """ raise exception """
+
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """ validates value """
+
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be grater than 0".format(name))
