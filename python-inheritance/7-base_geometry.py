42487a43a85a9a5451ff237d14bed2760b1b9dc8
Bryan Garcia <bags@Bryans-MacBook-Air.local>
2024-05-22 04:03:13 -0400

    added file

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -15,5 +15,5 @@ class BaseGeometry:
 
         if type(value) != int:
             raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
+        elif value <= 0:
             raise ValueError("{} must be grater than 0".format(name))
