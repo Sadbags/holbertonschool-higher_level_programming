ab63b1e0332898096e9bbc454e887de939595b7b
Bryan Garcia <bags@Bryans-MacBook-Air.local>
2024-05-28 11:44:28 -0400

    added file

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -3,7 +3,10 @@
 
 
 class Student:
+    """ student class """
+
     def __init__(self,first_name, last_name, age):
+        """ initialize the class """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
