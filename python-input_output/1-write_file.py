d11b5af3a483ae62e65c146dedcbcdc176d8dc3f
Bryan Garcia <bags@Bryans-MacBook-Air.local>
2024-05-28 10:39:28 -0400

    added file

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" write a string to text file and returns number of chars written """
+
+
+def write_file(filename="", text=""):
+    """ writing file """
+    with open(filename, 'w', encoding='utf-8') as file:
+        file.write(text)
+        return len(text)
+    
