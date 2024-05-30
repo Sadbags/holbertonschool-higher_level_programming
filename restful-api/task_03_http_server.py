429a923f917faccf5f1d58cbdf4ea49d50927eee
Bryan Garcia <bags@Bryans-MacBook-Air.local>
2024-05-30 16:35:20 -0400

    added file

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -41,7 +41,7 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
 def run(server_class=HTTPServer, handler_class=SimpleHTTPRequestHandler, port=8000):
     server_address = ('', port)
     httpd = server_class(server_address, handler_class)
-    print(f"Starting httpd server on port {port}...")
+    print(f"Starting httpd server on port {port}")
     httpd.serve_forever()
 
 if __name__ == "__main__":
