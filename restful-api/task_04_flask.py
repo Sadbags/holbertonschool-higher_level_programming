d36d99e44060ff7373569a6a35f062e61987984e
Bryan Garcia <bags@Bryans-MacBook-Air.local>
2024-05-31 10:07:21 -0400

    added file

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -15,7 +15,7 @@ def home():
 # Define the /data endpoint
 @app.route("/data")
 def data():
-    return jsonify(list(users.values()))
+    return jsonify(list(users.keys()))
 
 # Define the /status endpoint
 @app.route("/status")
@@ -25,19 +25,22 @@ def status():
 # Define the /users/<username> endpoint
 @app.route("/users/<username>")
 def get_user(username):
-    if username in users:
-        return jsonify(users[username])
+    user = users.get(username)
+    if user:
+        return jsonify(user)
     else:
-        return "User not found", 404
+        return jsonify({"error": "User nor found"}), 404
 
-# Define the /add_user endpoint to handle POST requests
 @app.route("/add_user", methods=["POST"])
 def add_user():
-    data = request.json
-    username = data["username"]
-    users[username] = data
-    return jsonify({"message": "User added", "user": data}), 201
+    new_user = request.json
+    username = new_user.get("username")
+    if not username:
+        return jsonify({"error": "User not found"}), 400
+    if username == users:
+        return jsonify({"error": "User already exists"}), 400
+    users[username] = new_user
+    return jsonify({"message": "User added", "user": new_user}), 201
 
-# Run the Flask app
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
