6a370a9873c9c9e5d4bc5e34d373e35f9f4097ce
Bryan Garcia <bags@Bryans-MacBook-Air.local>
2024-06-17 11:40:11 -0400

    added file

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,4 +1,5 @@
--- Lists all the number of records with the same score in the table
+-- Lists all the number of records with the same
+-- score in the table
 SELECT score, COUNT(score) AS NUMBER
 FROM second_table
 GROUP BY score
