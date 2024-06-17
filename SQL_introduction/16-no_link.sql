035377a21fdff768d313cdbc464e29c3a260d7fe
Bryan Garcia <bags@Bryans-MacBook-Air.local>
2024-06-17 11:47:23 -0400

    added file

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- Lists all records of the second table of the database
-SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score
