893b05a4b4498cb70eadb23ed391a4952c78d578
Bryan Garcia <bags@Bryans-MacBook-Air.local>
2024-06-18 09:58:59 -0400

    added file

---
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,8 @@
+-- list all shoes, and all genres linked to that show
+SELECT tv_shows.title AS title,
+    tv_genres.name AS genre
+FROM tv_shows
+    LEFT OUTER JOIN tv_shows_genres ON tv_shows.id = tv_show_genres.show_id
+	LEFT OUTER JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+ORDER BY title,
+    name;
