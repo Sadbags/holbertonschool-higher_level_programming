b06c85595143dc352b4c9ce45e6f376f323d3f24
Bryan Garcia <bags@Bryans-MacBook-Air.local>
2024-06-18 09:02:31 -0400

    added file

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -7,3 +7,10 @@ CREATE TABLE IF NOT EXISTS cities (
 	FOREIGN KEY(state_id) REFERENCES states(id),
 	name VARCHAR(256) NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS cities (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    state_id INT NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    FOREIGN KEY (state_id) REFERENCES states(id)
+);
