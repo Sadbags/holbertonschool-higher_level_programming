93acb44df11d651cfde791dc9ed8f719ce5ed9e2
Bryan Garcia <bags@Bryans-MacBook-Air.local>
2024-06-18 09:32:35 -0400

    added file

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,6 +1,5 @@
 -- lists all shows contained in the database
-SELECT tv_shows.title,
-    tv_show_genres.genre_id
+SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
     LEFT JOIN tv_show_genre ON tv_shows.id = tv_show_genre.show_id
 ORDER BY tv_shows.title, tv_show_genres.genre_id;
