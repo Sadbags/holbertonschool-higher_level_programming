842c25697f9c4ef31fa9567fd49888200c9cfd01
Bryan Garcia <bags@Bryans-MacBook-Air.local>
2024-06-18 09:59:51 -0400

    added file

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -4,5 +4,4 @@ SELECT tv_shows.title AS title,
 FROM tv_shows
     LEFT OUTER JOIN tv_shows_genres ON tv_shows.id = tv_show_genres.show_id
 	LEFT OUTER JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
-ORDER BY title,
-    name;
+ORDER BY title, name;
