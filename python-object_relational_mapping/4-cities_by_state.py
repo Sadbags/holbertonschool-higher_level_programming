2bb5622567e3b766cea33bf940049d780751e890
Bryan Garcia <bags@Bryans-MacBook-Air.local>
2024-06-19 13:03:20 -0400

    added file

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" script that lists al cities  """
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+
+    db = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3], charset='utf8')
+    cursor = db.cursor()
+    cursor.execute("SELECT cities.id, cities.name, states.name FROM cities,\
+        states WHERE states.id=state_id")
+    table = cursor.fetchall()
+    for row in table:
+        print(row)
+    cursor.close()
+    db.close()
