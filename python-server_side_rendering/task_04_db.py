0600a216aff82d421dba78df8892278f06ea3bfe
Bryan Garcia <bags@Bryans-MacBook-Air.local>
2024-07-15 23:30:47 -0400

    added files

--- python-server_side_rendering/task_04_db.py
+++ python-server_side_rendering/task_04_db.py
@@ -40,8 +40,10 @@ def products():
         products = read_json('products.json')
     elif source == 'csv':
         products = read_csv('products.csv')
-    else:
+    elif source =='sql':
         products = read_sqlite('products.db')
+    else:
+        return render_template('product_display.html', error="Wrong source")
 
     if product_id:
         product_id = int(product_id)
